18da1ae74704753d2bc79f25485ddcfb40cf85fe
andy1924 <deshpandearnavn@gmail.com>
2026-01-26 16:41:33 +0530

    EXP 3

---
+++ C035_EXP3_CG.cpp
@@ -0,0 +1,39 @@
+#include <graphics.h>
+#include <iostream>
+using namespace std;
+#define ROUND(a)  ((int)(a+0.5))
+
+void DDA(int xa, int ya, int xb, int yb)
+{
+    int gd = DETECT, gm;
+    initgraph(&gd, &gm);
+    int dx = xb - xa, dy = yb-ya, steps , k;
+    float xI , yI, x = xa, y = ya;
+
+    if(abs(dx)>abs(dy)) steps = abs(dx);
+    else steps = abs (dy);
+    xI = dx / float(steps);
+    yI = dy / float(steps);
+
+    putpixel (ROUND(x),ROUND(y),YELLOW);
+    cout<<"X: "<<ROUND(x)<<"Y: "<<ROUND(y)<<endl;
+
+    for (k = 0; k<steps; k++){
+        x += xI;
+        y += yI;
+        cout<<"X: "<<ROUND(x)<<"Y: "<<ROUND(y)<<endl;
+       delay(100);
+        putpixel (ROUND(x),ROUND(y),YELLOW);
+    }
+    outtextxy(200 , 200);
+    getch();
+    closegraph();
+}
+int main()
+{
+    int xa,ya,xb,yb;
+    cout<<"Enter the values for xa, ya, xb, yb :";
+    cin>>xa>>ya>>xb>>yb;
+    DDA(xa,ya,xb,yb);
+    return 0;
+}
